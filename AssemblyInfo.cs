fa412d24b979cc3760f4818d1a29a93ac0b1d123
Simon Cropp <simon.cropp@gmail.com>
2012-04-23 21:18:55 +1000

    make common version

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -2,9 +2,9 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UrbanAirship")]
-[assembly: AssemblyProduct("UrbanAirship")]
+[assembly: AssemblyTitle("UrbanBlimp")]
+[assembly: AssemblyProduct("UrbanBlimp")]
 [assembly: Guid("759d3e98-1be8-4aea-af0e-37ff7c5560c0")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: InternalsVisibleTo("UrbanBlimp.Tests")]
