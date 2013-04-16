c9c217926118a34a43e2216207f1915f7db8562f
Charlie Goddard <charlie@iamcharliegoddard.com>
2013-04-15 21:22:28 -0700

    added iSO Broadcast method to Urbanblimp #gkl

---
+++ UrbanBlimp.Tests/Apple/Broadcast/BroadcastNotificationRequestSerializerTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using UrbanBlimp.Apple;
+
+
+namespace UrbanBlimp.Tests.Apple
+{
+    [TestFixture]
+    public class BroadcastNotificationRequestSerializerTests
+    {
+        [Test]
+        public void Single()
+        {
+            var notification = new BroadcastNotificationRequest
+                                   {
+                                       Payload = new PushPayload
+                                                     {
+                                                         Alert = "My Alert",
+                                                         Sound = "My Sound",
+                                                         Badge = Badge.Increment()
+                                                     },
+                                       ExcludeTokens = new List<string> {"exclude1"},
+                                   };
+            var actual = notification.Serialize().FormatAsJson();
+            var expected = @"
+{
+  'aps': {
+    'alert': 'My Alert',
+    'badge': '+1',
+    'sound': 'My Sound'
+  },
+  'exclude_tokens': [
+    'exclude1'
+  ]
+}".Replace("\r\n", "\n");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ UrbanBlimp/Apple/Broadcast/BroadcastNotificationResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace UrbanBlimp.Apple
+{
+  public class BroadcastNotificationResponse
+  {
+  }
+}
