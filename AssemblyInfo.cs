303110533796e88fc61371ae1de3c215912a9e0a
Simon Cropp <simon.cropp@gmail.com>
2013-04-17 13:28:04 +1000

    Update AssemblyInfo.cs

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("UrbanBlimp")]
 [assembly: AssemblyProduct("UrbanBlimp")]
-[assembly: AssemblyVersion("1.12.0.1")]
-[assembly: AssemblyFileVersion("1.12.0.1")]
+[assembly: AssemblyVersion("1.13.0.0")]
+[assembly: AssemblyFileVersion("1.13.0.0")]
 [assembly: InternalsVisibleTo("UrbanBlimp.Tests")]
